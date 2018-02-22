1e7a1a286699da4c8ec63e6d3b7eae46c04544cb
Marat <voda-komfort@yandex.ru>
2018-02-22 21:26:55 +0000

    1

--- app.js
+++ app.js
@@ -148,6 +148,6 @@ var express    = require("express"),
 
     
     
-    app.listen(59899, process.env.IP, function(){
+    app.listen(process.env.PORT || 3000, process.env.IP, function(){
     console.log("Server has started!!!");
 });
